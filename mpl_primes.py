21a4820e282e3e14d695cbd2d836c23a2dd5c19b
Cam Crow <19ccrow99@gmail.com>
2023-02-26 18:33:45 -0600

    fix: removed savefig

--- mpl_primes.py
+++ mpl_primes.py
@@ -16,6 +16,6 @@ data = random.random((5,5))
 img = plt.imshow(data, interpolation='nearest')
 img.set_cmap('hot')
 plt.axis('off')
-plt.savefig("test.png", bbox_inches='tight')
+# plt.savefig("test.png", bbox_inches='tight')
 
 plt.show()
