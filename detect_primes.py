51136195c81439f4650e54c668fe213c6f3ff673
Cam Crow <19ccrow99@gmail.com>
2023-02-27 16:14:07 -0600

    feat: added detect prime function

---
+++ detect_primes.py
@@ -0,0 +1,32 @@
+# this function takes a number and evaluates whether the number is prime or not
+
+def detect_prime(num):
+    # two edge cases (1 & 2)
+    if num == 1:
+        print("false") 
+        return False
+    if num == 2: 
+        print("true")
+        return True
+
+    # modding by two first eliminates all even numbers immediately
+    if num % 2 == 0:
+        print("even", num)
+        return False
+
+
+    mod_num = 3
+    # iterates through all the odds up to the mid point
+    while mod_num < num / 2:
+        # if you have found a factor, the number is not prime
+        if num % mod_num == 0:
+            print("false", num, mod_num)
+            return False
+        # increase mod_num by two to go to the next prime number
+        mod_num += 2
+    
+    # the loop ended, so the number has to be prime!
+    print("true", num)
+    return True
+
+# detect_prime(15)
--- mpl_primes.py
+++ mpl_primes.py
@@ -1,23 +1,34 @@
-# import matplotlib.pyplot as plt
-
 # These are the first 9 numbers at cords (0,0)()
 # xcords = [0, 0, 1, 1, 1, 0, -1, -1, -1]
 # ycords = [0, 1, 1, 0, -1, -1, -1, 0, 1]
 
-# plt.plot(xcords, ycords, 'o')
-# plt.axis([0, 6, 0, 20])
 
 # plt.show()
 
-#test
-
 from numpy import random
 import matplotlib.pyplot as plt
+from detect_primes import *
 
-data = random.random((5,5))
-img = plt.imshow(data, interpolation='nearest')
-img.set_cmap('hot')
-plt.axis('off')
+detect_prime(1)
+detect_prime(2)
+detect_prime(3)
+detect_prime(4)
+detect_prime(5)
+detect_prime(6)
+detect_prime(7)
+detect_prime(8)
+detect_prime(9)
+detect_prime(10)
+detect_prime(11)
+detect_prime(12)
+
+# plt.plot(xcords, ycords, 'o')
+# plt.axis([0, 6, 0, 20])
+
+# data = random.random((5,5))
+# img = plt.imshow(data, interpolation='nearest')
+# img.set_cmap('hot')
+# plt.axis('off')
 # plt.savefig("test.png", bbox_inches='tight')
 
-plt.show()
+# plt.show()
