622f5eb4481820cbf63a42ad218c9b2a906427c6
Cam Crow <19ccrow99@gmail.com>
2023-02-28 10:19:42 -0600

    feat: added test cases

---
+++ _test.py
@@ -0,0 +1,14 @@
+from detect_primes import *
+
+def test_prime_one():
+    assert detect_prime(1) == False
+def test_prime_two():
+    assert detect_prime(2) == True
+def test_prime_three():
+    assert detect_prime(3) == True
+def test_prime_four():
+    assert detect_prime(99) == False
+def test_prime_five():
+    assert detect_prime(113) == True
+def test_prime_six():
+    assert detect_prime(119) == False
