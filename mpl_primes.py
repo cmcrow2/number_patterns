dfcbe2708009b30f28c4d8aefdfa681bfb073291
Cam Crow <19ccrow99@gmail.com>
2023-03-01 08:41:28 -0600

    feat: completed function to plot prime coords

--- mpl_primes.py
+++ mpl_primes.py
@@ -1,34 +1,18 @@
-# These are the first 9 numbers at cords (0,0)()
-# xcords = [0, 0, 1, 1, 1, 0, -1, -1, -1]
-# ycords = [0, 1, 1, 0, -1, -1, -1, 0, 1]
+# These are the first 9 numbers at cords: 
+# (0,0) (1,0) (1,1) (0,1) (-1,1) (-1,0) (-1,-1) (0,-1) (1,-1)
+# xcords = [0, 1, 1, 0, -1, -1, -1,  0,  1]
+# ycords = [0, 0, 1, 1,  1,  0, -1, -1, -1]
 
-
-# plt.show()
-
-from numpy import random
 import matplotlib.pyplot as plt
-from python_primes.global_functions.detect_primes import *
+from global_functions.order_nums import *
+from global_functions.detect_primes import *
 
-print(detect_prime(1))
-print(detect_prime(2))
-print(detect_prime(3))
-print(detect_prime(4))
-print(detect_prime(5))
-print(detect_prime(6))
-print(detect_prime(7))
-print(detect_prime(8))
-print(detect_prime(9))
-print(detect_prime(10))
-print(detect_prime(11))
-print(detect_prime(12))
+# add values to each list | always start @ 0,0
+xcords = []
+ycords = []
 
-# plt.plot(xcords, ycords, 'o')
-# plt.axis([0, 6, 0, 20])
+xcords, ycords = order_nums(25000)
+print(len(xcords))
 
-# data = random.random((5,5))
-# img = plt.imshow(data, interpolation='nearest')
-# img.set_cmap('hot')
-# plt.axis('off')
-# plt.savefig("test.png", bbox_inches='tight')
-
-# plt.show()
+plt.scatter(xcords, ycords, c="black")
+plt.show()
